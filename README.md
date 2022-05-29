36ca5f0c9ea4edd27ce7847a376415cf69bec34b
Ghis8 <ghislainkongolo@yahoo.com>
2022-05-29 13:28:55 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+here is an admin panel sample which has a light/dark mode
---
+++ index.js
@@ -0,0 +1,42 @@
+const sideMenu=document.querySelector(".aside")
+const menuBtn=document.querySelector("#menu-btn")
+const closeBtn=document.querySelector("#close-btn")
+const themeToggler=document.querySelector(".theme-toggler")
+
+menuBtn.addEventListener('click',()=>{
+	sideMenu.classList.add('hidden')
+})
+
+closeBtn.addEventListener('click',()=>{
+	sideMenu.classList.remove('hidden')
+})
+
+//close sidebar
+
+
+closeBtn.addEventListener('click',()=>{
+	sideMenu.style.display='none'
+})
+
+//change theme
+
+themeToggler.addEventListener('click',() =>{
+	document.body.classList.toggle('dark-theme-variables')
+	themeToggler.querySelector('span:nth-child(1)').classList.toggle('active')
+	themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
+})
+
+//fill orders table
+
+orders.forEach(order=>{
+	const tr=document.createElement('tr')
+	const trContent=`
+					<td>${order.productName}</td>
+					<td>${order.productNumber}</td>
+					<td>${order.paymentStatus}</td>
+					<td class="${order.shipping === 'Declined' ? 'danger' : order.shipping === 'pending'? 'warning' : 'primary'}">${order.shipping}</td>
+					<td class="primary">Details</td>
+	`
+	tr.innerHTML=trContent;
+	document.querySelector('table .order').appendChild(tr)
+})
